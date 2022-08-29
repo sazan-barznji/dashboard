2157620e3350db7b0aa528853c5a35d34b0a7cc6
sazan-barznji <sazanbarazenjiE@gmail.com>
2022-08-29 12:37:42 +0300

    c

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,6 +1,11 @@
 <template>
     <div class="login container">
         <form @submit.prevent="login" method="post">
+            <h1>Login</h1>
+            <div>
+                <h5>Login for New Account</h5>
+                <p>thank you for connecting with us, Login to manage your account</p>
+            </div>
             <div class="mb-3">
                 <label for="email" class="form-label">Email address</label>
                 <input type="email" class="form-control" id="email">
