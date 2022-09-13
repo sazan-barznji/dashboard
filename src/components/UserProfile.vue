a151c5bc4c25b9a344d90dd70eca46133159225f
sazan-barznji <sazanbarazenji@gmail.com>
2022-09-13 22:53:59 +0300

    commit

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,8 +1,25 @@
 <template>
-    <div>user profile</div>
+    <div>
+        <h1>user profile</h1>
+        <h2>{{userId}}</h2>
+    </div>
+
 </template>
 <script>
-    export default{
-        name:'UserProfile'
+export default {
+    name: 'UserProfile',
+    data() {
+        return {
+            userId: this.$route.params.userId
+        }
+    },
+    methods: {
+        checkID() {
+            this.userId = this.$route.params.userId
+        }
+    },
+    watch:{
+        $route: 'checkID'
     }
+}
 </script>
--- src/router/index.js
+++ src/router/index.js
@@ -44,7 +44,7 @@ const routes = [
     }
   },
   {
-    path: '/profile',
+    path: '/profile/:userId',
     name: 'UserProfile',
     component: UserProfile
   }
